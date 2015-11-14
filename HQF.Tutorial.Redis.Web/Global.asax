b6c70d85c60a8045a87ef94e8dac0080683edd43
huoxudong125 <423huo@163.com>
2015-11-14 18:29:56 +0800

    Add WebProject for test

---
+++ HQF.Tutorial.Redis.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HQF.Tutorial.Redis.Web.MvcApplication" Language="C#" %>
---
+++ HQF.Tutorial.Redis.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HQF.Tutorial.Redis.Web.Startup))]
+namespace HQF.Tutorial.Redis.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
